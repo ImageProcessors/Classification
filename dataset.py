b9a8d4f565969d4d4658d08bfb39d626c5e31701
mahdihosseinim <mahdi.hosseini.m81@gmail.com>
2025-07-15 18:14:55 +0330

    dataset | added my code

--- dataset.py
+++ dataset.py
@@ -1,134 +1,76 @@
-import ast
+import pandas as pd
+import os
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import python_calamine
+# import multiprocessing as mp
 import time
-from torch.utils.data import Dataset
-from PIL import Image
-import torchvision.transforms as transforms
-import polars as pl
+
+from concurrent.futures import ThreadPoolExecutor
 
 
-def convert_csv_to_parquet(csv_path, parquet_path):
-    df = pl.scan_csv(
-        csv_path,
-        dtypes={'path': pl.Utf8, 'label': pl.Utf8},
-        null_values=None,
-        low_memory=False
-    ).collect(streaming=True)
+def read_and_change_labels(singel_file):
     
-    df.write_parquet(  # comment
-            parquet_path,
-            compression='zstd',
-            compression_level=3,
-            use_pyarrow=True,
-            )
+    file = singel_file
+    root = "./excels"
 
+    # print(f"start processing of: {file}")    
 
-def read_path_labels(parquet_path):
-    """
-    Reads images paths  and their corresponding label or labels from the specified csv file.
-
-    Args:
-        csv_path (string): Root directory of csv file.
-
-    Returns:
-        tuple: A tuple containing:
-            - paths (list): A numpy array of image paths, each representing an image path.
-            - labels (list): A list containing list of labels for corresponded image.
-    Note: 
-        there must be no extra spaces between items in each line of csv file.
-    """
-    df = pl.read_parquet(
-        parquet_path,
-        use_pyarrow=True,
-        parallel=True,  # parallel reading
-        rechunk=False,  # جلوگیری از تخصیص مجدد حافظه
-        row_index_name=None,  # ther is no index row
-        low_memory=False  # use more memory
-    )
-    paths = df['path'].to_numpy()
-    labels = [ast.literal_eval(label) for label in df['label'].to_numpy()]
-
-    del df
-    return paths, labels
-
-
-class CustomDataset(Dataset):
-    """
-    Custom PyTorch Dataset class for loading images and labels from a directory structure.
-
-    Attributes:
-        paths (list): A list of image tensors loaded from the directory.
-        labels (list): A list of labels corresponding to each image.
-    """
-    
-    def __init__(self, csv_path: str, transform_: transforms.Compose):
-        """
-        Initializes the custom dataset by reading images paths and labels from the specified csv file.
-
-        Args:
-            csv_path (str): Root directory of csv file.
-            transform_ (torchvision.transforms.transforms.Compose): The transform to be applied to the images.
-        """
-
-        super().__init__()
-        self.paths, self.labels = read_path_labels(csv_path)
-        self.transform = transform_
+    df = pd.read_excel(file, engine="calamine", usecols=['ردیف پرونده', 'کد طبقه اول', 'کد طبقه دوم', 'کد طبقه سوم'], dtype=str)
         
+    df['im_path'] = root+ "/"+ df['ردیف پرونده'] + ".jpg"   # image add
 
-    def __len__(self):
-        """
-        Returns the number of samples in the dataset.
+    df['label code'] = df['کد طبقه اول'].astype(str) + '#' + df['کد طبقه دوم'].astype(str) + '#' + df['کد طبقه سوم'].astype(str)
 
-        Returns:
-            int: The total number of images (and labels) in the dataset.
-        """
+    df.drop(columns=['ردیف پرونده', 'کد طبقه اول', 'کد طبقه دوم', 'کد طبقه سوم'], inplace=True)
 
-        return len(self.paths)
-    
-    def __getitem__(self, index):
-        """
-        Retrieves the image and label at the specified index.
+    # print(f"{file} is completed")
 
-        Args:
-            index (int): The index of the data sample to retrieve.
+    return df
 
-        Returns:
-            dict: A dictionary containing:
-                - 'image': The image tensor at the specified index.
-                - 'label': Labels (list) corresponding to the image.
-        """
-                
-        try:
-            with Image.open(self.paths[index]) as img:
-                
-                if img.mode != 'RGB':
-                    img = img.convert('RGB')
-                                
-                return {
-                    'image': self.transform(img),
-                    'label': self.labels[index]  # cab contain single or several labels
-                }
-        except Exception as e:
-            print(f"error while opening image at index {index}, path: {self.paths[index]}")
-            raise e 
-            # return {
-            #         'image': torch.zeros(3, 224, 224),
-            #         'label': torch.zeros_like(self.labels_tensor[0])
-            #     }
+def save_csv(name, df):
+    start = time.time()
+    df.to_csv(name, encoding='utf-8-sig', index=False)
+    print(f"{name} saved in {time.time() - start:.2f} seconds.")
+
+
+
+def managing_dataset(root, exel_paths):
+    all_data_frames = []
+    print(exel_paths)
+
+    print("start threading")
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        all_data_frames = list(executor.map(read_and_change_labels, exel_paths))
+
+    # print("------------------")
+    # print("ended threading")
+    # print("start merging")
+
+    merged_df = pd.concat(all_data_frames, ignore_index=True)
+
+    unique_labels = {label_code: idx for idx, label_code in enumerate(merged_df['label code'].unique())} 
+    merged_df['label'] = merged_df['label code'].map(unique_labels)
+
+    seed = 30
+
+    # print("--------------")
+    # print("start spliting")
+    train_df, temp_df = train_test_split(merged_df, test_size=0.2, random_state=seed, shuffle=True)
+    valid_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=seed, shuffle=True)
+
+    # print("--------------")
+    # print("start to csv")   
+
+    train_df.to_csv('train.csv', encoding='utf-8-sig', index=False)
+    test_df.to_csv('test.csv', encoding='utf-8-sig', index=False)
+    valid_df.to_csv('validation.csv', encoding='utf-8-sig', index=False)
 
 
 if __name__ == "__main__":
-    t0 = time.time()
-    
-    convert_csv_to_parquet("test_data.csv", "test.parquet")
-    
     t1 = time.time()
-    
-    print(f"converting csv to parquet took: {t1-t0:.3f} seconds")
-    
-    ds = CustomDataset("test.parquet", transforms.Compose([transforms.ToTensor()]))
-    
-    for i in range(ds.__len__()):
-        ds[i]
+    root = "./excels"
+    exel_paths = list(map(os.path.normpath, list(map(str, Path(root).rglob('*.xlsx')))))
+    managing_dataset(root, exel_paths)
     t2 = time.time()
-    
-    print(f"processing {ds.__len__()} images took: {t2-t1:.3f} seconds")
+    print(f"execute time: {t2 - t1}")
